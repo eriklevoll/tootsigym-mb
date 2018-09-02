5a9753ed3596e3598f979c31aa96e5bf2127659b
eriklev <erik.levoll@gmail.com>
2018-09-02 16:13:14 +0300

    added initial advertisments

--- TootsiGymMB/app/src/main/java/com/example/erik_spectre/tootsigymmb/Controller/MainActivity.kt
+++ TootsiGymMB/app/src/main/java/com/example/erik_spectre/tootsigymmb/Controller/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.erik_spectre.tootsigymmb.Controller
 
 import android.Manifest
+import android.bluetooth.*
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -12,17 +12,20 @@ import android.view.MenuItem
 import com.example.erik_spectre.tootsigymmb.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Build
 import android.support.v4.content.PermissionChecker
 import com.example.erik_spectre.tootsigymmb.Model.BLE
-import com.example.erik_spectre.tootsigymmb.Utilities.ADAPTER_STATE_ON
-import com.example.erik_spectre.tootsigymmb.Utilities.REQUEST_BLUETOOTH
-import com.example.erik_spectre.tootsigymmb.Utilities.REQUEST_COARSE_LOCATION
 import kotlinx.android.synthetic.main.nav_header_main.*
 import java.util.*
 import kotlin.concurrent.schedule
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.content.Context
+import android.os.ParcelUuid
+import android.util.Log
+import com.example.erik_spectre.tootsigymmb.Utilities.*
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -49,6 +52,154 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         BleConnection = BLE(this)
         BleConnection.Init()
+
+    }
+
+    private fun startAdvertising() {
+        val bleManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val adapter = bleManager.adapter
+        val advertiser = adapter.bluetoothLeAdvertiser
+
+        adapter.name = DEVICE_NAME
+
+        val settings = AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                .setConnectable(true)
+                .build()
+
+        val advData = AdvertiseData.Builder()
+                .addServiceUuid(ParcelUuid(UUID.fromString(DEVICE_SERVICE_UUID)))
+                .build()
+
+        val advScanResponse = AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .build()
+
+        val advCallback = object : AdvertiseCallback() {
+
+            override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+                super.onStartSuccess(settingsInEffect)
+                println("Bluetooth Advertise success.")
+
+            }
+
+            override fun onStartFailure(errorCode: Int) {
+                super.onStartFailure(errorCode)
+                when (errorCode) {
+                    ADVERTISE_FAILED_ALREADY_STARTED -> println("ADVERTISE_FAILED_ALREADY_STARTED")
+                    ADVERTISE_FAILED_DATA_TOO_LARGE -> println("ADVERTISE_FAILED_DATA_TOO_LARGE")
+                    ADVERTISE_FAILED_FEATURE_UNSUPPORTED -> println("ADVERTISE_FAILED_FEATURE_UNSUPPORTED")
+                    ADVERTISE_FAILED_INTERNAL_ERROR -> println("ADVERTISE_FAILED_INTERNAL_ERROR")
+                    ADVERTISE_FAILED_TOO_MANY_ADVERTISERS -> println("ADVERTISE_FAILED_TOO_MANY_ADVERTISERS")
+                    else -> println("Advertise failed. Errorcode: $errorCode")
+                }
+            }
+        }
+
+        advertiser.startAdvertising(settings, advData, advScanResponse, advCallback)
+        val server = bleManager.openGattServer(this, gattServerCallback)
+        server.addService(createGATTService())
+        println("start advertising")
+    }
+
+    fun createGATTService(): BluetoothGattService {
+        val service = BluetoothGattService(UUID.fromString(MOONBOARD_DATA_SERVICE_UUID),
+                BluetoothGattService.SERVICE_TYPE_PRIMARY)
+
+        val chr = BluetoothGattCharacteristic(UUID.fromString(MOONBOARD_DATA_CHAR_UUID),
+                BluetoothGattCharacteristic.PROPERTY_NOTIFY or
+                        BluetoothGattCharacteristic.PROPERTY_WRITE or
+                BluetoothGattCharacteristic.PROPERTY_READ,
+                BluetoothGattCharacteristic.PERMISSION_READ or
+                BluetoothGattCharacteristic.PROPERTY_WRITE or
+                BluetoothGattCharacteristic.PROPERTY_NOTIFY)
+        service.addCharacteristic(chr)
+        return service
+    }
+
+    fun stopAdvertising() {
+        val adapter = BluetoothAdapter.getDefaultAdapter()
+        val advertiser = adapter.bluetoothLeAdvertiser
+
+        val advCallback = object : AdvertiseCallback() {}
+        advertiser.stopAdvertising(advCallback)
+    }
+
+    fun startBleGATTServer() {
+        val bleManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothGattServer = bleManager.openGattServer(this, gattServerCallback)
+    }
+
+    /**
+     * Callback to handle incoming requests to the GATT server.
+     * All read/write requests for characteristics and descriptors are handled here.
+     */
+
+    private var bluetoothGattServer: BluetoothGattServer? = null
+
+    private val gattServerCallback = object : BluetoothGattServerCallback() {
+
+        override fun onConnectionStateChange(device: BluetoothDevice, status: Int, newState: Int) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                println("BluetoothDevice CONNECTED: $device")
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                println("BluetoothDevice DISCONNECTED: $device")
+                //Remove device from any active subscriptions
+                //registeredDevices.remove(device)
+            }
+        }
+
+        override fun onCharacteristicReadRequest(device: BluetoothDevice, requestId: Int, offset: Int,
+                                                 characteristic: BluetoothGattCharacteristic) {
+            val now = System.currentTimeMillis()
+            when (characteristic.uuid.toString()){
+                "345ue89sduh3784235" -> {
+                    println("Read Char")
+                    bluetoothGattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, byteArrayOf(1,2,3))
+                }
+                else -> {
+                    // Invalid characteristic
+                    println("Invalid Characteristic Read: ${characteristic.uuid}")
+                    bluetoothGattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_FAILURE, 0, null)
+                }
+            }
+        }
+
+        override fun onDescriptorReadRequest(device: BluetoothDevice, requestId: Int, offset: Int,
+                                             descriptor: BluetoothGattDescriptor) {
+            if (descriptor.uuid.toString() == "something") {
+                println("Config descriptor read")
+                BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                //BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE
+                bluetoothGattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, byteArrayOf(1,2,3))
+            } else {
+                println("Unknown descriptor read request")
+                bluetoothGattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_FAILURE, 0, null)
+            }
+        }
+
+        override fun onDescriptorWriteRequest(device: BluetoothDevice, requestId: Int,
+                                              descriptor: BluetoothGattDescriptor,
+                                              preparedWrite: Boolean, responseNeeded: Boolean,
+                                              offset: Int, value: ByteArray) {
+            if (descriptor.uuid.toString() == "something") {
+                if (Arrays.equals(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE, value)) {
+                    println("Subscribe device to notifications: $device")
+                } else if (Arrays.equals(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE, value)) {
+                    println("Unsubscribe device from notifications: $device")
+                }
+
+                if (responseNeeded) {
+                    bluetoothGattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, null)
+                }
+            } else {
+                println("Unknown descriptor write request")
+                if (responseNeeded) {
+                    bluetoothGattServer?.sendResponse(device, requestId, BluetoothGatt.GATT_FAILURE, 0, null)
+                }
+            }
+        }
     }
 
     fun ClosedRange<Int>.random() = Random().nextInt((endInclusive + 1) - start) +  start
@@ -147,7 +298,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 waitAdapterInit()
             }
         } else {
-            BleConnection.startScan()
+            //BleConnection.startScan()
+            startAdvertising()
         }
     }
 
--- TootsiGymMB/app/src/main/java/com/example/erik_spectre/tootsigymmb/Utilities/Constants.kt
+++ TootsiGymMB/app/src/main/java/com/example/erik_spectre/tootsigymmb/Utilities/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.erik_spectre.tootsigymmb.Utilities
 
+const val DEVICE_NAME = "MOONBOARD_APP"
+
 const val MOONBOARD_SERVICE_UUID        = "12EFFA29-0B6E-40B3-9181-BE9509B23200"
 
 const val REQUEST_BLUETOOTH         = 0
@@ -9,6 +11,8 @@ const val REQUEST_COARSE_LOCATION   = 1
 const val MOONBOARD_MAC                 = "24:0A:C4:00:04:32"
 const val MOONBOARD_DATA_SERVICE_UUID   = "4880c12c-fdcb-4077-8920-a450d7f9b907"
 const val MOONBOARD_DATA_CHAR_UUID      = "afc13ec4-6d71-4442-9f81-55bc21d658d6"
+
+const val DEVICE_SERVICE_UUID           = "12EFFA29-0B6E-40B3-9181-BE9509B23200"
 
 const val NUM_OF_ROWS = 18
 const val NUM_OF_COLS = 11
