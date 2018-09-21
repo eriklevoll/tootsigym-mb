3fd3caad62652d4ce083b445bffae3b886af94b4
eriklev <erik.levoll@gmail.com>
2018-09-21 16:01:49 +0300

    code cleanup

--- TootsiGymMB/app/src/main/java/com/example/erik_spectre/tootsigymmb/Utilities/BleHelpers.kt
+++
@@ -1,6 +0,0 @@
-//package com.example.erik_spectre.tootsigymmb.Utilities
-//
-//import android.bluetooth.BluetoothGattService
-//import com.beepiz.bluetooth.gattcoroutines.experimental.GattConnection
-//
-//typealias GattBasicUsage = (GattConnection, List<BluetoothGattService>) -> Unit
