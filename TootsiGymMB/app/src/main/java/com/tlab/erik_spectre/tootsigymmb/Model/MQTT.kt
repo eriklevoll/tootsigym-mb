64d881bd0f7b68fb0365ee8d8432c2b3e48f4e46
eriklev <erik.levoll@gmail.com>
2018-09-24 15:22:01 +0300

    added new mqtt topic for responses

--- TootsiGymMB/app/src/main/java/com/tlab/erik_spectre/tootsigymmb/Model/MQTT.kt
+++ TootsiGymMB/app/src/main/java/com/tlab/erik_spectre/tootsigymmb/Model/MQTT.kt
@@ -1,6 +1,7 @@
 package com.tlab.erik_spectre.tootsigymmb.Model
 
 import android.content.Context
+import android.util.Log
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
 
@@ -17,11 +18,31 @@ class MQTT(private val context: Context,
 
     fun init() {
         client = MqttAndroidClient(context, "tcp://$address:$port", clientId)
+        client.setCallback(MQTTConnectionCallback)
         connectionOptions = MqttConnectOptions()
         connectionOptions.userName = username
         connectionOptions.password = password.toCharArray()
         println("Im here!")
         startConnection()
+    }
+
+    private object MQTTConnectionCallback: MqttCallbackExtended {
+        override fun connectComplete(b: Boolean, s: String) {
+            println("Connect complete, $s")
+        }
+
+        override fun connectionLost(throwable: Throwable) {
+            println("Connect lost")
+        }
+
+        @Throws(Exception::class)
+        override fun messageArrived(topic: String, mqttMessage: MqttMessage) {
+            println("messageArrived, $mqttMessage")
+        }
+
+        override fun deliveryComplete(iMqttDeliveryToken: IMqttDeliveryToken) {
+            println("deliveryComplete")
+        }
     }
 
     private fun startConnection()
@@ -45,7 +66,7 @@ class MQTT(private val context: Context,
 
     private fun subscribeToTopic() {
         try {
-            client.subscribe("moon", 0)
+            client.subscribe("moon_resp", 0)
             println("Success subscribe")
             publishToTopic("Connected")
         } catch (e: MqttException) {
@@ -63,7 +84,7 @@ class MQTT(private val context: Context,
         }
     }
 
-    public fun sendData(data: String) {
+    fun sendData(data: String) {
         publishToTopic(data)
     }
 }
